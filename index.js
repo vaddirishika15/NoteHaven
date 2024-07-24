b2d906074c0c4315c4acbdfbc2195c5665dbe8cf
VADDI RISHIKA <120818025+vaddirishika15@users.noreply.github.com>
2024-07-24 22:36:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 const notesRoutes = require('./routes/notes');
 const serverless = require('serverless-http');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
@@ -15,6 +16,16 @@ connectDB();
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+// Set up CORS
+const corsOptions = {
+    origin: '*', // You can specify the allowed origins here
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
+
+// Routes
 app.use('/notes', notesRoutes);
 
 // Export the app to be used by serverless
@@ -24,6 +35,6 @@ module.exports.handler = serverless(app);
 // Optional: Keep the app running for local development
 if (process.env.NODE_ENV !== 'production') {
     app.listen(PORT, () => {
-        console.log(`Server is running fine on port : ${PORT}`);
+        console.log(`Server is running fine on port: ${PORT}`);
     });
 }
